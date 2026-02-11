ee4cb990ef12b9ba74a6ea9151fca465749643bc
Hammad Ahmed <syedhammad283@gmail.com>
2026-02-12 00:49:44 +0500

    jws

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import { router } from "./routes/userRoutes.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/food")
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("runnig");
+});
+
+app.use("/api", router);
+
+
+app.use(authMiddleware);
+app.get("/api/products", async (req, res) => {
+  res.send("Products");
+});
+
+app.listen(port, () => {
+  console.log(`Server listing on port ${port}`);
+});
